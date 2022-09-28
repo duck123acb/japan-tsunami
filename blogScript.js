00640121a31342439597ed09b8c47406b33e16cd
Duck123Acb <95136800+duck123acb@users.noreply.github.com>
2022-09-28 15:35:27 -0600

    Home page look good

---
+++ blogScript.js
@@ -0,0 +1,49 @@
+document.getElementById("toggle-going-to-spain").addEventListener("click", function() {
+    if (document.getElementById("going-to-spain").style.display === "inline") {
+        document.getElementById("going-to-spain").style.display = "none";
+        document.getElementById("toggle-going-to-spain").innerHTML = "<strong>+</strong>";
+    } else {
+        document.getElementById("going-to-spain").style.display = "inline";
+        document.getElementById("toggle-going-to-spain").innerHTML = "<strong>-</strong>";
+    }
+});
+
+document.getElementById("toggle-second-adventure").addEventListener("click", function() {
+    if (document.getElementById("second-adventure").style.display === "inline") {
+        document.getElementById("second-adventure").style.display = "none";
+        document.getElementById("toggle-second-adventure").innerHTML = "<strong>+</strong>";
+    } else {
+        document.getElementById("second-adventure").style.display = "inline";
+        document.getElementById("toggle-second-adventure").innerHTML = "<strong>-</strong>";
+    }
+});
+
+document.getElementById("toggle-this-might-be-the-last-post").addEventListener("click", function() {
+    if (document.getElementById("this-might-be-the-last-post").style.display === "inline") {
+        document.getElementById("this-might-be-the-last-post").style.display = "none";
+        document.getElementById("toggle-this-might-be-the-last-post").innerHTML = "<strong>+</strong>";
+    } else {
+        document.getElementById("this-might-be-the-last-post").style.display = "inline";
+        document.getElementById("toggle-this-might-be-the-last-post").innerHTML = "<strong>-</strong>";
+    }
+});
+
+document.getElementById("toggle-were-safe").addEventListener("click", function() {
+    if (document.getElementById("were-safe").style.display === "inline") {
+        document.getElementById("were-safe").style.display = "none";
+        document.getElementById("toggle-were-safe").innerHTML = "<strong>+</strong>";
+    } else {
+        document.getElementById("were-safe").style.display = "inline";
+        document.getElementById("toggle-were-safe").innerHTML = "<strong>-</strong>";
+    }
+});
+
+document.getElementById("toggle-this-might-be-the-last-post-again").addEventListener("click", function() {
+    if (document.getElementById("this-might-be-the-last-post-again").style.display === "inline") {
+        document.getElementById("this-might-be-the-last-post-again").style.display = "none";
+        document.getElementById("toggle-this-might-be-the-last-post-again").innerHTML = "<strong>+</strong>";
+    } else {
+        document.getElementById("this-might-be-the-last-post-again").style.display = "inline";
+        document.getElementById("toggle-this-might-be-the-last-post-again").innerHTML = "<strong>-</strong>";
+    }
+});

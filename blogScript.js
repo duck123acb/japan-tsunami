42eb1ad892927bcb9ec60506905f31ad0f385677
Duck123Acb <95136800+duck123acb@users.noreply.github.com>
2022-09-28 18:11:36 -0600

    apperanty you cant use a foreach on that

--- blogScript.js
+++ blogScript.js
@@ -53,15 +53,14 @@ const buttons = document.getElementsByClassName("blog-button");
 const texts = document.getElementsByClassName("blog-text");
 let textIndex = 0;
 
-buttons.forEach(button => {
-    button.addEventListener("click", function() {
-        if (texts[textIndex].style.display === "inline") {
-            texts[textIndex].style.display = "none";
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        if (texts[i].style.display === "inline") {
+            texts[i].style.display = "none";
             button.innerHTML = "<strong>+</strong>";
         } else {
-            texts[textIndex].style.display = "inline";
+            texts[i].style.display = "inline";
             document.getElementById("toggle-this-might-be-the-last-post-again").innerHTML = "<strong>-</strong>";
         }
     });
-    textIndex++;
-});
+};
